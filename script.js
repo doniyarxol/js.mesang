b0b0fc907b5702772b1778d3c146f58ef79687db
doniyarxol <132245867+doniyarxol@users.noreply.github.com>
2025-06-27 19:04:16 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = prompt("Your name") 
+let b = prompt("Your age")
+let c = prompt("Your email")
+let e = prompt("Your ")
+let f = prompt("Yoqtirgan fanini")
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(e);
+console.log(f);
